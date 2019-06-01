fce0ecad093cc89b027ae7b70f0f19ad29f95a33
lequocdien <lequocdien1998@gmail.com>
2019-06-02 00:09:37 +0700

    Modified RouteConfig

--- OnlineShop/OnlineShop/App_Start/RouteConfig.cs
+++ OnlineShop/OnlineShop/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace OnlineShop
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Test", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Test", action = "Index", id = UrlParameter.Optional }, namespaces: new [] { "OnlineShop.Controllers" }
             );
         }
     }
